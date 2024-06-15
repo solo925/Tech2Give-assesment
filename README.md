19b035b06dab0fbc25c23be080b6bade3ac2a40c
davinci <davinci@localhost.localdomain>
2024-06-15 10:40:24 +0300

    first question

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tech2Give-assesment
---
+++ question1.py
@@ -0,0 +1,15 @@
+# Question 1: Design a function that reverses the digits of an integer. 
+# For example, 50 should become 5 and -12 should become -21.
+
+def reverse_integer(number):
+    #  I converted the integer to a string and checked if it's negative
+    sign = -1 if number < 0 else 1
+    number_str = str(abs(number))
+    
+    # I reverse the string and converted it back to an integer with the correct sign
+    reversed_number = int(number_str[::-1]) * sign
+    return reversed_number
+
+# I then tested the string to accertain my solution
+print(reverse_integer(50))   # expected output: 5
+print(reverse_integer(-12))  # expected output: -21
